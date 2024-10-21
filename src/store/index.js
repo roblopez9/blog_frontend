9443072a0ffca249f51fe9f064b033dbe9774dc1
roblopez9 <robertrl9supreme@gmail.com>
2024-10-21 15:29:00 -0400

    adding statemangement

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
 
-const useStore = create(devtools((set) => {
+const useStore = create(devtools(immer((set) => {
   return {
     count: 0,
     // careful with this syntax - if 2nd arg is true it will replace all state rather than merge
-    increment: () => set((state) => ({ count: state.count + 1 }), false, 'count/increment'),
-    decrement: () => set((state) => ({ count: state.count - 1 }), false, 'count/decrement'), 
+    increment: () => set((draftState) => { draftState.count += 1; }, false, 'count/increment'),
+    decrement: () => set((draftState) => { draftState.count -= 1; }, false, 'count/decrement'),
   };
-}));
+})));
 
 export default useStore;
